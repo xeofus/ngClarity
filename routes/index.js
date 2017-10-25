f012cb4299107909fa9f58fe756c76e58ba02adb
Adilet Abdi <xeofus@gmail.com>
2017-10-25 16:42:48 +0600

    refactoring

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by xeofus on 25.10.2017.
- */
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-  res.render('index.html');
-});
-module.exports = router;
--- server.js
+++ server.js
@@ -2,34 +2,12 @@
  * Created by xeofus on 25.10.2017.
  */
 "use strict";
-// const express = require('express'),  app = express(),  path = require('path');
-//
-// app.use(express.static(__dirname));
-//
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'dist/'));
-// });
-//
-// app.listen(process.env.PORT || 5000);
+const express = require('express'),  app = express(),  path = require('path');
 
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var index = require('./routes/index');
-// app
-var app = express();
-// cors
-// views. -- VERY IMPORTANT
-app.set('views', path.join(__dirname, 'src'));
-// engine
-app.set('view enginer', 'ejs');
-app.engine('html', require('ejs').renderFile);
-// angular  dist -- VERY IMPORTANT
-app.use(express.static(__dirname + '/dist'));
-// body bodyParser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-// routes
-app.use('/', index);
-// port.
-var server = app.listen(process.env.PORT || 5000);
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/'));
+});
+
+app.listen(process.env.PORT || 5000);
