0308b3a707cd848fc9714c6c3e042626aec4b333
Adilet Abdi <xeofus@gmail.com>
2017-10-25 16:29:11 +0600

    refactoring

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express'),
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/'));
 });
 
 app.listen(process.env.PORT || 5000);
