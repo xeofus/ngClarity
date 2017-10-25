02e5a74adde76ade974b0ab3c90019d295f1236a
Adilet Abdi <xeofus@gmail.com>
2017-10-25 16:18:32 +0600

    refactoring

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname));
 app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(portNumber, function () {
